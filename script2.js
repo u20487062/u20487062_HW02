5935e7166338ebca26f4195809fbf6f836089d28
u20487062 <u20487062@tuks.co.za>
2022-05-08 00:07:17 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,21 +1,27 @@
 var storedNames = JSON.parse(localStorage.getItem("selectMovies"))
 console.log(storedNames)
+console.log(storedNames.length)
 
-document.getElementById("tbody").innerHTML += `<tr>
-<td>
-    <button class="fa fa-close" style="border-style: hidden; background-color: white; color: red;"></button>
-    ${storedNames[0].title}</td>
-<td id="tableUnitPrice">120</td>
-<td>
-    <button class="fa fa-toggle-left" style="border-style: hidden; background-color: white;" onclick="decreaseQuantity()"></button>
-    <div id="quantityAmount">0</div>
-    <button class="fa fa-toggle-right" style="border-style: hidden; background-color: white;" onclick="increaseQuantity()"></button>
-</td>
-<td id="rowTotal">
-</td>
-</tr>`
+window.onload = function(){
 
-let oQuantity = 0;
+for(var i = 0; i < storedNames.length; i++){
+    document.getElementById("tbody").innerHTML += `<tr>
+                                                        <td>
+                                                                <button class="fa fa-close" style="border-style: hidden; background-color: white; color: red;"></button>
+                                                                ${storedNames[i].title}
+                                                        </td>
+                                                        <td id="tableUnitPrice">${storedNames[i].price}</td>
+                                                        <td>
+                                                                <button class="fa fa-toggle-left" style="border-style: hidden; background-color: white;" onclick="decreaseQuantity()"></button>
+                                                                <div id="quantityAmount">${storedNames[i].amount}</div>
+                                                                <button class="fa fa-toggle-right" style="border-style: hidden; background-color: white;" onclick="increaseQuantity()"></button>
+                                                        </td>
+                                                        <td id="rowTotal"></td>
+                                                    </tr>`
+}
+}
+
+/*let oQuantity = 0;
 let total = 0;
 const quantity = document.getElementById("quantityAmount");
 const stringquantity = document.getElementById("quantityAmount").textContent;
@@ -46,4 +52,4 @@ function rowTotal(){
 console.log(stringquantity)
 console.log(intquantity)
 console.log(unitPrice)
-console.log(intquantity*unitPrice)
+console.log(intquantity*unitPrice)*/
